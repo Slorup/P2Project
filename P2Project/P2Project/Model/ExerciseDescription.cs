ca06177dabc6871b147f8575401d58bd18e82e62
Slorup <nicklas.slorup@yahoo.dk>
2019-04-11 09:39:42 +0200

    ExerciseDescription

---
+++ P2Project/P2Project/Model/ExerciseDescription.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace P2Project.Model
+{
+    class ExerciseDescription
+    {
+        public string TextDescription { get; set; }
+        public string VideoPath { get; set; }
+        public string AudioPath { get; set; }
+        public List<string> ImagePaths { get; set; }
+
+        public ExerciseDescription(string textDescription, string videoPath, string audioPath, List<string> imagePaths)
+        {
+            TextDescription = textDescription;
+            VideoPath = videoPath;
+            AudioPath = audioPath;
+            ImagePaths = imagePaths;
+        }
+
+        public ExerciseDescription(string textDescription) : this(textDescription, null, null, null) { }
+        public ExerciseDescription(string textDescription, string videoPath) : this(textDescription, videoPath, null, null) { }
+        //TODO
+    }
+}
