a8045cb92c9752604de452fde6b9657750d301d6
kasper skaarup <kskskaarup@gmail.com>
2019-04-11 11:56:45 +0200

    mere gennemse

--- P2Project/P2Project/ViewModel/MakeExerciseViewModel.cs
+++ P2Project/P2Project/ViewModel/MakeExerciseViewModel.cs
@@ -20,30 +20,83 @@ namespace P2Project.ViewModel
             get { return _textBlock1; }
             set { SetProperty(ref _textBlock1, value); }
         }
+        private string _textBlock2;
 
+        public string TextBlock2
+        {
+            get { return _textBlock2; }
+            set { SetProperty(ref _textBlock2, value); }
+        }
+        private string _textBlock3;
+
+        public string TextBlock3
+        {
+            get { return _textBlock3; }
+            set { SetProperty(ref _textBlock3, value); }
+        }
 
         public MakeExerciseViewModel(User currentUser)
         {
             CurrentUser = currentUser;
         }
-        private ICommand _browseCommand;
-        public ICommand BrowseCommand
+        private ICommand _browseCommandVideo;
+        public ICommand BrowseCommandVideo
         {
             get
             {
-                return _browseCommand ?? (_browseCommand = new RelayCommand(param => BrowseCommandClick(param)));
+                return _browseCommandVideo ?? (_browseCommandVideo = new RelayCommand(param => BrowseCommandVideoClick(param)));
             }
         }
 
-        private void BrowseCommandClick(object param)
+        private void BrowseCommandVideoClick(object param)
         {
             OpenFileDialog fileDialog = new OpenFileDialog();
-            fileDialog.DefaultExt = ".txt"; // Required file extension 
-            fileDialog.Filter = "Text documents (.txt)|*.txt"; // Optional file extensions
+            fileDialog.DefaultExt = ".txt";
+            fileDialog.Filter = "Text documents (.txt)|*.txt";
 
             fileDialog.ShowDialog();
             TextBlock1 = fileDialog.FileName;
             
+        }
+
+        private ICommand _browseCommandSound;
+        public ICommand BrowseCommandSound
+        {
+            get
+            {
+                return _browseCommandSound ?? (_browseCommandSound = new RelayCommand(param => BrowseCommandSoundClick(param)));
+            }
+        }
+
+        private void BrowseCommandSoundClick(object param)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.DefaultExt = ".txt";
+            fileDialog.Filter = "Text documents (.txt)|*.txt"; 
+
+            fileDialog.ShowDialog();
+            TextBlock2 = fileDialog.FileName;
+
+        }
+
+        private ICommand _browseCommandImage;
+        public ICommand BrowseCommandImage
+        {
+            get
+            {
+                return _browseCommandImage ?? (_browseCommandImage = new RelayCommand(param => BrowseCommandImageClick(param)));
+            }
+        }
+
+        private void BrowseCommandImageClick(object param)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.DefaultExt = ".txt"; 
+            fileDialog.Filter = "Text documents (.txt)|*.txt";
+
+            fileDialog.ShowDialog();
+            TextBlock3 = fileDialog.FileName;
+
         }
     }
 }
