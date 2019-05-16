ce694ab1f4e2d788a16970f1a8a736bab386ebf7
Slorup <nicklas.slorup@yahoo.dk>
2019-05-16 09:31:34 +0200

    Script error is gone 🦀

--- P2Project/P2Project/View/ExercisePage.xaml.cs
+++ P2Project/P2Project/View/ExercisePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,15 @@ namespace P2Project.View
         public ExercisePage()
         {
             InitializeComponent();
+        }
+
+        private void ExerciseViewer_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            dynamic activeX = this.ExerciseViewer.GetType().InvokeMember("ActiveXInstance",
+                BindingFlags.GetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+                null, this.ExerciseViewer, new object[] { });
+
+            activeX.Silent = true;
         }
     }
 }
